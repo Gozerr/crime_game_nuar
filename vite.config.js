d5fa6f9ef90c5124c849cf289ff6c6b6e8197bf2
Gozerr <32794649+Gozerr@users.noreply.github.com>
2026-08-08 21:12:11 +0300

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  base: '/crime_game_nuar/',
   plugins: [vue()],
 })
